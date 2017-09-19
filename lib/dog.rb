32cc551be613b133b66af5c69bf6ac669ecf7d77
Xochitl Fierros <xochitlfm@gmail.com>
2017-09-19 23:47:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,6 @@
 class Dog
-  def initialize(name)
-    @name = name
+ def initialize(name, breed="Mutt")
+   @name = name
+   @breed = breed
+ end
 end
-
-def name=(name)
-  @name = name
-end
-
-def name
-  @name
-end
-
-end
-
- 
